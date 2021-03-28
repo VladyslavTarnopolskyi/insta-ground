caf5bb6e91cc41a36d8dae50b2a71f91ea4feca4
vlad.tarnopolskyi <vlad.tarnopolskiy.ne@gmail.com>
2021-03-28 18:01:06 +0300

    edited server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require("express");
 const app = express();
-const router = express.Router();
-const http = require('http');
-const PORT = process.env.PORT || 5000;
-const serverless = require('serverless-http');
+// const router = express.Router();
+// const http = require('http');
+// const PORT = process.env.PORT || 5000;
+// const serverless = require('serverless-http');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use('/.netlify/functions/server', router);
+// app.use('/.netlify/functions/server', router);
 app.use('/oauth/access_token', createProxyMiddleware(
   {
     target: 'https://api.instagram.com',
@@ -16,15 +16,14 @@ app.use('/oauth/access_token', createProxyMiddleware(
   }
   ));
 
-app.set('trust proxy', 'loopback')
+// app.set('trust proxy', 'loopback')
 
 app.get(['/*'], (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-const httpServer = http.createServer({}, app);
-
-httpServer.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// const httpServer = http.createServer({}, app);
+//
+// httpServer.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 module.exports = app;
-module.exports.handler = serverless(app);
