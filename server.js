e06204b0e5b3eeffbe6bba66b670736bad6c2533
vlad.tarnopolskyi <vlad.tarnopolskiy.ne@gmail.com>
2021-04-11 16:36:54 +0300

    new load 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require("express");
 const app = express();
 // const router = express.Router();
-// const http = require('http');
+const http = require('http');
 const PORT = process.env.PORT || 5000;
 // const serverless = require('serverless-http');
 const { createProxyMiddleware } = require('http-proxy-middleware');
@@ -22,6 +22,8 @@ app.get(['/*'], (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
+const httpServer = http.createServer({}, app);
+//
 httpServer.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 module.exports = app;
