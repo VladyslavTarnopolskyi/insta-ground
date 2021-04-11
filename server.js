76ffab548ffb2c610aacff7e6548d2e66e3e07e1
vlad.tarnopolskyi <vlad.tarnopolskiy.ne@gmail.com>
2021-04-11 16:32:58 +0300

    new load

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const path = require('path');
 const express = require("express");
 const app = express();
+// const router = express.Router();
+// const http = require('http');
 const PORT = process.env.PORT || 5000;
+// const serverless = require('serverless-http');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 app.use(express.static(path.join(__dirname, 'dist')));
+// app.use('/.netlify/functions/server', router);
 app.use('/oauth/access_token', createProxyMiddleware(
   {
     target: 'https://api.instagram.com',
@@ -12,10 +16,12 @@ app.use('/oauth/access_token', createProxyMiddleware(
   }
   ));
 
+// app.set('trust proxy', 'loopback')
+
 app.get(['/*'], (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+httpServer.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 module.exports = app;
